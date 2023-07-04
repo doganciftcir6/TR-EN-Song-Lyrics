ad9a63cd76a91842447766ea6d55c4ecee75bfdd
doganciftcir6 <114991786+doganciftcir6@users.noreply.github.com>
2023-07-04 15:22:12 +0300

    CreateCategoryController

---
+++ Backoffice/controllers/CategoryContoller.js
@@ -0,0 +1,105 @@
+const Category = require("../models/CategoryModel");
+const mongoose = require("mongoose");
+
+//!GetAll
+module.exports.GetAllCategories = async (req, res) => {
+  await Category.find()
+    .then((categories) => {
+      res.status(200).json(categories);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+//!GetById
+module.exports.GetByIdCategories = async (req, res) => {
+  //id urlden gelecek
+  const categoryId = req.params.id;
+  //verilen id mongoose'un varsayılan id özelliklerine göre (ObjectId'ye göre) geçerlimi veya var mı
+  if (!categoryId || !mongoose.Types.ObjectId.isValid(categoryId)) {
+    return res.status(400).json("Invalid category ID!!");
+  }
+
+  await Category.findById(categoryId)
+    .then((category) => {
+      if (category) {
+        res.status(200).json(category);
+      } else {
+        res.status(404).json("Category not found!");
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+//!Insert
+module.exports.InsertCategory = async (req, res) => {
+  const newCategory = new Category(req.body);
+  await newCategory
+    .save()
+    .then(() => {
+      res
+        .status(200)
+        .json(`${newCategory._id} category with ID successfully added.`);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+//!Update
+module.exports.UpdateCategory = async (req, res) => {
+  //id bu sefer bodyden gelecek
+  const updatedCategory = req.body;
+  //verilen id mongoose'un varsayılan id özelliklerine göre (ObjectId'ye göre) geçerlimi veya var mı
+  //BU KOD YAPISINI HELPER METOTA AL TEKRARA DÜŞTÜ!!!!!!
+  if (
+    !updatedCategory._id ||
+    !mongoose.Types.ObjectId.isValid(updatedCategory._id)
+  ) {
+    return res.status(400).json("Invalid category ID!!");
+  }
+
+  await Category.findOneAndUpdate(
+    { _id: updatedCategory._id }, // Güncellenecek kategorinin ID'si
+    updatedCategory, // Güncelleme verileri
+    { runValidators: true } // validation kontrollerinin yapılması için
+  )
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json("Category not found!");
+      }
+      res
+        .status(200)
+        .json(`${result._id} category with ID successfully updated.`);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
+
+//!Delete
+module.exports.DeleteCategory = async (req, res) => {
+  //id urlden gelecek
+  const categoryId = req.params.id;
+  //verilen id mongoose'un varsayılan id özelliklerine göre (ObjectId'ye göre) geçerlimi veya var mı
+  //BU KOD YAPISINI HELPER METOTA AL TEKRARA DÜŞTÜ!!!!!!
+  if (!categoryId || !mongoose.Types.ObjectId.isValid(categoryId)) {
+    return res.status(400).json("Invalid category ID!!");
+  }
+
+  await Category.findByIdAndDelete(categoryId)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json("Category not found!");
+      }
+      res
+        .status(200)
+        .json(`${result._id} category with ID successfully deleted.`);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+};
--- Backoffice/server.js
+++ Backoffice/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const ConnectDB = require("./Helpers/DBHelper/ConnectMongoDB");
+const categoryRoutes = require("./routes/CategoryRoutes");
 
 
 const app = express();
@@ -11,8 +13,14 @@ dotenv.config();
 //db baglan
 ConnectDB(process.env.DB_CONNECTION_STRING, process.env.PORT, app);
 
+//!middlewears
+//gelen isteklerin gövdesindeki JSON verilerini okuyabilmek. (body verileri)
+app.use(express.json());
+//tüm isteklere CORS yanıtlarını etkinleştirir yani tüm isteklerin izin verildiği anlamına gelir. (gelen isteklerin izinlerini kontrol etmek için.)
+app.use(cors());
 
-//routes
 
+//!routes
+app.use("/api/categories", categoryRoutes);
 
 
