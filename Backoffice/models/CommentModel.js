6e58c83da14d7ce40c9972132eee4a9db17a5975
doganciftcir6 <114991786+doganciftcir6@users.noreply.github.com>
2023-07-04 12:47:32 +0300

    CreateCommentModel

---
+++ Backoffice/models/CommentModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+//timestamps: true ile kayıdın oluşturulma tarihini tut.
+const commentSchema = mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: [true, "You must enter an content!"],
+      minlength: [3, "The username field must have a minimum of 3 characters!"],
+      maxlength: [
+        200,
+        "The username field must have a maximum of 200 characters!",
+      ],
+    },
+    //relations
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "You must select a user!"],
+    },
+    song: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Song",
+      required: [true, "You must select a song!"],
+    },
+    status: {
+      type: Boolean
+    }
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comments", commentSchema);
+module.exports = Comment;
